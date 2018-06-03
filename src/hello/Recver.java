ee92f27b1f10e4e9afbb5e2d99adef18dc4b1724
chong <fqyyang@163.com>
2018-06-03 12:57:00 -0700

    set up initial vesion of simple productioner-consumer

---
+++ src/hello/Recver.java
@@ -0,0 +1,64 @@
+package hello;
+
+//
+//                            _ooOoo_  
+//                           o8888888o  
+//                           88" . "88  
+//                           (| -_- |)  
+//                            O\ = /O  
+//                        ____/`---'\____  
+//                      .   ' \\| |// `.  
+//                       / \\||| : |||// \  
+//                     / _||||| -:- |||||- \  
+//                       | | \\\ - /// | |  
+//                     | \_| ''\---/'' | |  
+//                      \ .-\__ `-` ___/-. /  
+//                   ___`. .' /--.--\ `. . __  
+//                ."" '< `.___\_<|>_/___.' >'"".  
+//               | | : `- \`.;`\ _ /`;.`/ - ` : | |  
+//                 \ \ `-. \_ __\ /__ _/ .-` / /  
+//         ======`-.____`-.___\_____/___.-`____.-'======  
+//                            `=---='  
+//  
+//         .............................................  
+//                  佛祖镇楼                  BUG辟易  
+//          佛曰:  
+//                  写字楼里写字间，写字间里程序员；  
+//                  程序人员写程序，又拿程序换酒钱。  
+//                  酒醒只在网上坐，酒醉还来网下眠；  
+//                  酒醉酒醒日复日，网上网下年复年。  
+//                  但愿老死电脑间，不愿鞠躬老板前；  
+//                  奔驰宝马贵者趣，公交自行程序员。  
+//                  别人笑我忒疯癫，我笑自己命太贱；  
+//  
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.QueueingConsumer;
+import uitl.ConnextionUtil;
+
+/**
+ * Created by chong
+ */
+public class Recver {
+    private final static String QUEUE = "MQ_SCUT1";//队列的名字
+
+    public static void main(String[] args) throws Exception{
+        Connection connection = ConnextionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE,false,false,false,null);
+
+        //定义一个消费者,QueueingConsumer已经过时,建议使用DefaultConsumer子类
+        QueueingConsumer consumer =new QueueingConsumer(channel);
+        //接收消息 ,参数2 是自动确认
+        channel.basicConsume(QUEUE, true, consumer);
+
+        while (true) {
+            //获取消息: 如果没有消息会等待,有的话就获取执行然后销毁,是一次性的
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            String message = new String(delivery.getBody());
+            System.out.println("收到了一条消息:---" + message);
+
+        }
+    }
+}
